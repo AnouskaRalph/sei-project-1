f3c603e59cf5ed818994067aea64b50868e33e18
Anouska Ralph <anouskaoleary@gmail.com>
2020-08-27 17:07:45 +0100

    img

--- scripts/app.js
+++ scripts/app.js
@@ -1,37 +1,39 @@
 function init() {
 
   const grid = document.querySelector('.grid')
-  const aliens = document.querySelectorAll('.aliens')
-  console.log(aliens)
+  console.log(grid)
+
+
+  //console.log('aliens', aliens)
   
   //Grid 
   const cells = []
   const width = 10
   const cellCount = width * width
-  let aliensPosition = 0
-  
+
+  let alienCurrentPosition = 0
+  let gunCurrentPosition = 0 
+
+
   function createGrid() {
     for (let i = 0; i < cellCount; i++) {
       const cell = document.createElement('div')
       cell.textContent = i
       grid.appendChild(cell)
       cells.push(cell)
-    }     
+    }      
   }
-createGrid()
-
-//ADDING ALIENS 
-
-function addAliens(position) {
-  cells[position].classList.add('aliens')
-}
-addAliens(position)
+  createGrid()
 
 
 
 
-aliens.forEach(alien => alien.addEventListener('window', placeAliens))
 
+
+
+
+
+  
 
 
 
