3b5283b7c31b5ed246c0ff7676675c5af86dcbeb
Anouska Ralph <anouskaoleary@gmail.com>
2020-08-28 10:41:09 +0100

    adding vamps

--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,61 @@
 function init() {
 
   const grid = document.querySelector('.grid')
-  console.log('grid', grid)
- 
   
   //Grid 
   const cells = []
   const width = 10
   const cellCount = width * width
+  let gunPosition = 90
+  let vampPosition = 11
 
+  function addGun(position) {
+    cells[position].classList.add('gun')
+  }
+  function removeGun(position) {
+    cells[position].classList.remove('gun')
+  } 
+  function addVamp(postion) {
+    cells[position].classList.add('vamp')
+  }
 
-
-  function createGrid() {
+  function createGrid(startingPosition) {
     for (let i = 0; i < cellCount; i++) {
       const cell = document.createElement('div')
       cell.textContent = i
       grid.appendChild(cell)
       cells.push(cell)
-    }    
+    }  
+    addGun(startingPosition)  
   }
  
-  createGrid()
-
-
-
-
+  
+  function handleKeyUp(event) {
+  
+    removeGun(gunPosition) // * remove gun from the current position
+  
+    const x = gunPosition % width // if gun / width has no remainder then dont move him left or right
+      
+    switch (event.keyCode) { 
+      case 39: //arrow right
+        if (x < width - 1) gunPosition++
+        break
+      case 37: //arrow left
+        if (x > 0) gunPosition--
+        break
+      default:
+        console.log('invalid key do nothing')
+    }
+  
+    addGun(gunPosition) // add gun back at the new position
+  }
+  
+  
+  createGrid(gunPosition)
+  
+  // Event listeners
+  //keyup - when lift up keydown - when press down it fires the event
+  document.addEventListener('keyup', handleKeyUp)
 
 
 
