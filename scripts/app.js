fc0d7ee91a92301e055f1d56399e24e14b1f13b9
Anouska Ralph <anouskaoleary@gmail.com>
2020-08-30 10:43:38 +0100

    adding heart

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,7 @@ function init() {
   const width = 10
   const cellCount = width * width
   const graves = [81, 83, 85, 87, 89]
-  const vamps = [
+  let vamps = [
     11, 12, 13, 14, 15, 16, 17, 18,
     21, 22, 23, 24, 25, 26, 27, 28,
     31, 32, 33, 34, 35, 36, 37, 38]
@@ -20,32 +20,25 @@ function init() {
   
 
 
-  function moveVamps() {
-    let count = 0 
-    let timerId = setInterval(() => {
-    
-    vamps.forEach(vamp) => {
-    let result = vamps++ 
-    vampPosition = result
-    }
-
-    }, 3000)
-  }
-  moveVamps()
-
   function addGun(position) {
     cells[position].classList.add('gun')
   }
   function removeGun(position) {
     cells[position].classList.remove('gun')
   } 
-  function addVamp(vamp) {
+  function addVamps(vamp) {
     // for each vamps in the array, access each vamp, and do, cells add vamp and add this class vamp to it. 
     vamps.forEach((vamp, i) => {
       cells[vamp].classList.add('vamp')
     })
   } 
-  function addGrave(grave) {
+  function removeVamps(vamp) {
+    vamps.forEach((vamp, i) => {
+      cells[vamp].classList.remove('vamp')
+    })
+  } 
+
+  function addGraves(grave) {
     graves.forEach((grave, i) => {
       cells[grave].classList.add('grave')
     })
@@ -60,8 +53,23 @@ function init() {
       cells.push(cell)
     }     
     addGun(startingPosition)
-    addVamp(vampPosition)  
-    addGrave(gravePosition)
+    addVamps(vampPosition)  
+    addGraves(gravePosition)
+  }
+
+  
+
+
+  function moveVamps() {
+    let count = 0 
+    let timerId = setInterval(() => {
+      removeVamps()
+      vamps = vamps.map(vamp => {
+        return vamp + 1
+      })
+      addVamps()
+
+    }, 1000)
   }
 
   
@@ -85,10 +93,7 @@ function init() {
     addGun(gunPosition) // add gun back at the new position
   }
   
-  // function handleClick() {
-  
-  // }
-  // handleClick()
+
 
 
 
@@ -97,11 +102,6 @@ function init() {
 
   startButton.addEventListener('click', moveVamps)
   document.addEventListener('keyup', handleKeyUp)
-
-
-
-
-
 
 
 
