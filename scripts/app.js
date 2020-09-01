8558f9c53521093d5cd7a50252591134952d990b
Anouska Ralph <anouskaoleary@gmail.com>
2020-09-01 17:16:30 +0100

    heart flying a go go, score keeping first attemp

--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,32 @@
 function init() {
-
-
+  
   const grid = document.querySelector('.grid')
   const startButton = document.querySelector('.button')
   const gameOver = document.querySelector('#game-over')
+  const scoreDisplay = document.querySelector('#score-display')
+  
   const cells = []
   const width = 10
   const cellCount = width * width
   const graves = [81, 83, 85, 87]
 
-
+  
   let vamps = [ 1, 2, 3, 4, 5, 6, 7, 8, 
     11, 12, 13, 14, 15, 16, 17, 18,
-    21, 22, 23, 24, 25, 26, 27, 28, 28]  
+    21, 22, 23, 24, 25, 26, 27, 28, 28]
+  // const newHeartPosition = Math.floor(Math.random() * vamps.length)
+  let score = 0
   let gunPosition = 90
   let vampPosition
   let gravePosition
   let swordPosition
   let heartPosition
-  let newHeartPosition
+  
   let moveVampsTimer
   let moveSwordTimer
   let moveHeartTimer
-
-
-
+  
+  
   function addGun(position) {
     cells[position].classList.add('gun')
   }
@@ -32,7 +34,7 @@ function init() {
   function removeGun() {
     cells[gunPosition].classList.remove('gun')
   } 
-
+  
   function addSword() {
     if (cells[swordPosition].classList.contains('grave')) {
       clearInterval(moveSwordTimer)
@@ -49,51 +51,58 @@ function init() {
       cells[swordPosition].classList.add('sword')
     }
   }
-
-
+  
   function removeSword() {
     cells[swordPosition].classList.remove('sword')
   } 
-
+  
   function addVamps() {
     vamps.forEach((vamp) => {
       if (cells[vamp].classList.contains('grave')) {
         endGame()
       } else
-        cells[vamp].classList.add('vamp')    
+        cells[vamp].classList.add('vamp')  
     })
   } 
-
+  
   function removeVamps() {
     vamps.forEach((vamp) => {
       cells[vamp].classList.remove('vamp')
     })
   } 
-
-
+  
   function addGraves() {
     graves.forEach((grave) => {
       cells[grave].classList.add('grave')
     })
   }
+  
   function removeGraves() {
     graves.forEach((grave) => {
       cells[grave].classList.remove('grave')
     })
   }
-
-  function addHearts() {    
-    // const newHeartPosition = vamps[(Math.floor(Math.random() * vamps.length))]
-    // cells[newHeartPosition].classList.add('heart')
-    // console.log(newHeartPosition) 
-    console.log('WORKINGGGGG')
+  
+  function addHearts() {  
+    if (cells[heartPosition].classList.contains('grave')) {
+      // console.log('CONTAINS GRAVE 222')
+      clearInterval(moveHeartTimer)
+      removeHearts()
+      graves.splice(graves.indexOf(heartPosition), 1)
+      cells[heartPosition].classList.remove('grave')
+      
+    } else {
+      cells[heartPosition].classList.add('heart')
+    }
+    //console.log('newHeartPosition', cells[newHeartPosition])
+    //console.log(newHeartPosition) 
+    //console.log('WORKINGGGGG')
   } 
   
   function removeHearts() {
     cells[heartPosition].classList.remove('heart')
   }
   
-
   function createGrid(startingPosition) {
     for (let i = 0; i < cellCount; i++) {
       const cell = document.createElement('div')
@@ -103,20 +112,20 @@ function init() {
     addGun(startingPosition)
     addVamps(vampPosition)  
     addGraves(gravePosition)
-    
   }  
-
-  function moveVamps() {    
+  
+  function moveVamps() { 
+    moveHeart()
     moveVampsTimer = setInterval(() => {
       removeVamps()
+      //It doesn't work here
       vamps = vamps.map(vamp => {
         return vamp + 1
       })
-      addVamps()       
+      addVamps()
     }, 1000)
   }
-
-
+  
   function moveSword() {
     swordPosition = gunPosition
     moveSwordTimer = setInterval(() => {
@@ -126,22 +135,29 @@ function init() {
     }, 100)
   }
   
+  function getNewHeartPosition() {
+    return Math.floor(Math.random() * vamps.length)
+  }
   function moveHeart() {
-    heartPosition = newHeartPosition
+    heartPosition = getNewHeartPosition()
     moveHeartTimer = setInterval(() => {
       removeHearts()
-      heartPosition = newHeartPosition - 10 
+      heartPosition = heartPosition + 10 
       addHearts()
+      console.log('WORKING EVERY 1SECOND')
     }, 1000)
-  }
+  } 
 
+  function scoreKeeping() {
+
+  }
+  
   function endGame() {
     removeVamps() 
     clearInterval(moveVampsTimer)
     gameOver.innerHTML = 'Game Over'
-
   }
-
+  
   function handleKeyUp(event) {
     removeGun() 
     const x = gunPosition % width // if gun / width has no remainder then dont move him left or right
@@ -160,16 +176,12 @@ function init() {
     }
     addGun(gunPosition) 
   }
-
-
-
-
+  
+  
   createGrid(gunPosition, vampPosition, gravePosition)
-  startButton.addEventListener('click', moveVamps, moveHeart)
+  startButton.addEventListener('click', moveVamps)
   // startButton.addEventListener('hoover', )
   document.addEventListener('keyup', handleKeyUp)
-
-
 
 
 
